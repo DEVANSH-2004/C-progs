82522efa17fbb8e481050856bf117dee95916a02
DEVANSH-2004 <120578873+DEVANSH-2004@users.noreply.github.com>
2023-01-27 10:42:18 +0530

    Add files via upload

--- recursion-factorial-calc.c
+++ recursion-factorial-calc.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+int fact(int);
 
 int main()
 {
+    int n,s;
+    printf("Enter a number for its factorial calculation=\n");
+    scanf("%d",&n);
+    s=fact(n);
+    printf("The factorial of %d is =%d \n",n,s);
     
-    printf("Enter a number for its factorial calculation");
+    }
+
+int fact(int a)
+{
+    int p=1;
+    if(a==1)
+    {
+        return(1);  //it defines that fact(1)=1 the all the values of fact are likewise calculated
+
+    }
+    else{
+        p=a*fact(a-1);
+    }
+    return(p);
 }
